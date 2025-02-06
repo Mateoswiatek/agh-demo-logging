88907dbcd4ccca8a3e083e4ac2e19b048558b4ab
mswiatek <mswiatek@core-logic.com>
2025-02-06 16:05:24 +0100

    init

---
+++ src/test/java/pl/swmatiass/demo/logging/DemologgingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swmatiass.demo.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemologgingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
